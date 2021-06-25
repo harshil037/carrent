16cef9d18b004c9e891feda214855a58d255d233
sufyanmansuri <60652760+sufyanmansuri@users.noreply.github.com>
2021-06-25 15:58:28 +0530

    mysql connection
    added superuser username : admin
    password : dipudipakdilip

--- carrent/settings.py
+++ carrent/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'carrent.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'carrent',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
