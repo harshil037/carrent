daacb3849f0ba1238df828c5832c52b1aee31690
harshil037 <patelharshil037@gmail.com>
2021-06-25 12:32:45 +0530

    template add
    templates url  add in views  path nd url

--- car/urls.py
+++ car/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from car import views
 
+
 urlpatterns = [
-    path("", views.index, name='car'),
+    path("", views.index, name='index'),
+    path("about", views.about, name='about'),
+    path("team", views.team, name='team'),
+    path("faq", views.faq, name='faq'),
+    path("fleet", views.fleet, name='fleet'),
+    path("offers", views.offers, name='offers'),
+    path("terms", views.terms, name='terms'),
+    path("testimonials", views.testimonials, name='testimonials'),
+
+    #contact pandenig
 ]
--- car/views.py
+++ car/views.py
@@ -4,3 +4,26 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def faq(request):
+    return render(request, 'faq.html')
+
+def fleet(request):
+    return render(request, 'fleet.html')
+
+def offers(request):
+    return render(request, 'offers.html')
+
+def terms(request):
+    return render(request, 'terms.html')
+
+def testimonials(request):
+    return render(request, 'testimonials.html')
+
+# contact pending
