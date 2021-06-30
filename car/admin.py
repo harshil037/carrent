c65ba593ecff2629a541e1bb871ab3c56b037ad8
harshil037 <patelharshil037@gmail.com>
2021-06-30 22:18:24 +0530

    AdminContact
    contact fatched in admin panel

--- car/admin.py
+++ car/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from car.models import Contact
 
 # Register your models here.
+
+def getConatactModel(model):
+    return [field.name for field in model._meta.get_fields()]
+
+class ContactDetail(admin.ModelAdmin):
+    list_display = getConatactModel(Contact)
+
+admin.site.register(Contact, ContactDetail)
