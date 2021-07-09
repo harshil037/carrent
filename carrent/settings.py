33336e891ec1a2692388f4d84d23c07da1f71053
Sufyan Mansuri <sufyan8834@gmail.com>
2021-07-10 03:45:24 +0530

    Email works

--- carrent/settings.py
+++ carrent/settings.py
@@ -142,7 +142,7 @@ MEDIA_URL = '/image/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.google.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'sufyan8834@gmail.com'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
